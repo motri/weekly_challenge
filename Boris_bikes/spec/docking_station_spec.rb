2819a8671ade75ff136ef396f710e5649ecddd28
Unai Motriko <unaimotriko@gmail.com>
2017-04-14 20:43:10 +0100

    Release_bike tdd working?

--- Boris_bikes/spec/docking_station_spec.rb
+++ Boris_bikes/spec/docking_station_spec.rb
@@ -1,5 +1,11 @@
 require "docking_station"
 
 describe DockingStation do
-    it {is_expected.to respond_to :release_bike}
+  it {is_expected.to respond_to :release_bike}
+
+  it 'releases working bikes' do
+    bike = Bike.new
+    expect(bike).to be_working
+  end
+
 end
