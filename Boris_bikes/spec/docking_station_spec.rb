f72ac4275cbd2be351daa67bdff978d072c9385b
Unai Motriko <unaimotriko@gmail.com>
2017-04-14 20:12:52 +0100

    £

--- Boris_bikes/spec/docking_station_spec.rb
+++ Boris_bikes/spec/docking_station_spec.rb
@@ -1,7 +1,5 @@
 require "docking_station"
 
 describe DockingStation do
-  it "releases a bike" do
-    expect(subject).to respond_to :release_bike
-  end
+    it {is_expected.to respond_to :release_bike}
 end
