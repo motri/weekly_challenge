5629bb41ceb419b498993be93bb1aac350722029
Unai Motriko <unaimotriko@gmail.com>
2017-04-17 22:11:07 +0100

    step 8

--- oystercard/lib/oystercard.rb
+++ oystercard/lib/oystercard.rb
@@ -6,16 +6,29 @@ class Oystercard
 
   def initialize
     @balance = 0
+    @status = false
   end
 
   def top_up(n)
-    fail "That exceeds the maximum balance" if
-   (balance + n) > MAX_BALANCE
+    raise 'That exceeds the maximum balance' if
+    (balance + n) > MAX_BALANCE
     @balance += n
   end
 
   def deduct(m)
     @balance -= m
+  end
+
+  def in_journey?
+    @status
+  end
+
+  def touch_in
+    @status = true
+  end
+
+  def touch_out
+    @status = false
   end
 
 end
--- oystercard/spec/oystercard_spec.rb
+++ oystercard/spec/oystercard_spec.rb
@@ -1,7 +1,6 @@
 require 'oystercard'
 
 describe Oystercard do
-
   it 'checks the cards balance' do
     expect(subject.balance).not_to eq nil
   end
@@ -11,21 +10,45 @@ describe Oystercard do
     it { is_expected.to respond_to(:top_up).with(1).argument }
 
     it 'tops up your balance' do
-      expect{ subject.top_up 5 }.to change{ subject.balance }.by 5
+      expect { subject.top_up 5 }.to change { subject.balance }.by 5
     end
 
-    it 'raises an error when trying to exceed max balance' do
-      message = "That exceeds the maximum balance"
-      expect{ subject.top_up Oystercard::MAX_BALANCE + 1 }.to raise_error message
+    it 'raises an error when cleartrying to exceed max balance' do
+      message = 'That exceeds the maximum balance'
+      subject.top_up(Oystercard::MAX_BALANCE)
+      expect { subject.top_up 1 }.to raise_error message
     end
+  end
 
     describe '#deduct' do
-
       it 'deducts fare from balance' do
-        expect{ subject.deduct 5 }.to change { subject.balance }.by -5
+        expect { subject.deduct 5 }.to change { subject.balance }.by -5
       end
     end
 
-  end
+    describe '#in_journey?' do
+      it 'starts as not in journey'do
+        expect(subject).to_not be_in_journey
+      end
+    end
 
-end
+    describe '#touch_in' do
+      it { is_expected.to respond_to(:touch_in) }
+
+      it "loggs when touched in" do
+        subject.touch_in
+        expect(subject).to be_in_journey
+      end
+    end
+
+    describe '#touch_out' do
+
+      it { is_expected.to respond_to(:touch_out) }
+
+      it 'loggs when touched out' do
+          subject.touch_in
+          subject.touch_out
+          expect(subject).to_not be_in_journey
+      end
+    end
+  end
